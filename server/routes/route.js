df05e63ec48ae0438b9299ac828ec06795be5f02
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-06-22 01:51:53 +0530

    added login routes for backend server

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { registerController } from "../controllers/registerController.js";
+import { loginController } from "../controllers/loginController.js";
 import { upload } from "../upload.js";
 import { uploadController } from "../controllers/uploadController.js";
 
@@ -7,5 +8,6 @@ const routes = express.Router();
 
 routes.post("/upload", upload.single("profileImage"), uploadController);
 routes.post("/register", registerController);
+routes.post("/login", loginController);
 
 export default routes;

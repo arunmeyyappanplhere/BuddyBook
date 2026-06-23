fff485a19b821e6f64abfcc163a55aa1775c164e
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-06-23 23:28:57 +0530

    added time stamps

--- server/models/contactModal.js
+++ server/models/contactModal.js
@@ -1,44 +1,46 @@
 import mongoose from "mongoose";
 
-const contactModal = mongoose.Schema({
-    contact_uid : {
-        type:String,
-        required:true,
-        default : crypto.randomUUID(),
+const contactModal = mongoose.Schema(
+  {
+    contact_uid: {
+      type: String,
+      required: true,
+      default: crypto.randomUUID(),
     },
-    savedUser : {
-        type:String,
-        required:true,
+    savedUser: {
+      type: String,
+      required: true,
     },
-    contact_name : {
-        type:String,
-        required:true,
-    }, 
-    contact_role : {
-        type : String,
-        required:true,
+    contact_name: {
+      type: String,
+      required: true,
     },
-    contact_email:{
-        type : String,
-        required:true,
+    contact_role: {
+      type: String,
+      required: true,
     },
-    contact_phone : {
-        type : Number,
-        required:true,
+    contact_email: {
+      type: String,
+      required: true,
     },
-    contact_dob : {
-        type : Date,
-        required:true,
+    contact_phone: {
+      type: Number,
+      required: true,
     },
-    contact_relation:{
-        type : String,
-        required:true,
+    contact_dob: {
+      type: Date,
+      required: true,
     },
-    contact_address : {
-        type : String,
-        required:true,
-    }
+    contact_relation: {
+      type: String,
+      required: true,
+    },
+    contact_address: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
-})
-
-export default mongoose.model("contacts", contactModal)
+export default mongoose.model("contacts", contactModal);

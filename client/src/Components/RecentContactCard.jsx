9f04782a7550cc1525cfd60cc5ed884856494341
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-07-06 23:13:02 +0530

    Made dynamic for props

--- client/src/Components/RecentContactCard.jsx
+++ client/src/Components/RecentContactCard.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import defaultImage from "/default_avatar.png";
-const RecentContactCard = () => {
+const RecentContactCard = ({
+  contactName = "User",
+  role = "User role",
+  contactNumber = 1234567890,
+  contactImage,
+}) => {
   return (
     <div className="bg-[#f9f9f934] w-full shadow-xs rounded-sm flex gap-10 items-center py-1 px-4 cursor-pointer">
-      <img src={defaultImage} alt="" className="size-15 rounded-full" />
+      <img
+        src={contactImage ? "" : defaultImage}
+        alt=""
+        className="size-15 rounded-full"
+      />
       <div>
-        <h1 className="font-medium text-md">Arun Meyyappan P L</h1>
-
-        <h2 className="text-gray-500 text-sm text-wrap">
-          Full Stack Website Development at Zoho 
-        </h2>
+        <h1 className="font-medium text-md">{contactName}</h1>
+        <h2 className="text-gray-500 text-sm text-wrap">{role}</h2>
       </div>
-      <h2 className="font-medium text-sm ml-auto text-nowrap">90426 49000</h2>
+      <h2 className="font-medium text-sm ml-auto text-nowrap">
+        {contactNumber}
+      </h2>
     </div>
   );
 };

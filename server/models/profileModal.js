f1113cc4cf7895de372d701ab06e820ab4bb6f7c
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-07-06 23:16:45 +0530

    Created Profile Modal for Dashboard page ; need refactor on users modal

--- server/models/profileModal.js
+++ server/models/profileModal.js
@@ -1,30 +1,39 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
-const profileModal = new mongoose.Schema({
-  profileImage: {
-    type: String,
-    required: true,
+const profileModal = new mongoose.Schema(
+  {
+    uuid: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    contacts: {
+      type: Array,
+      required: false,
+      default: [],
+    },
+    favorites: {
+      type: Array,
+      required: false,
+      default: [],
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: {
-    type: Number,
-    required: true,
-  },
-  contacts: {
-    type: Array,
-    required: false,
-  },
-  favorites: {
-    type: Array,
-    required: false,
-  },
-}, {timestamps:true});
+  { timestamps: true },
+);
 
-export default mongoose.modal("profiles", profileModal)
+export default mongoose.model("profiles", profileModal);
--- server/routes/route.js
+++ server/routes/route.js
@@ -3,11 +3,13 @@ import { registerController } from "../controllers/registerController.js";
 import { loginController } from "../controllers/loginController.js";
 import { upload } from "../upload.js";
 import { uploadController } from "../controllers/uploadController.js";
+import { homeController } from "../controllers/homeController.js";
 
 const routes = express.Router();
 
 routes.post("/upload", upload.single("profileImage"), uploadController);
 routes.post("/register", registerController);
 routes.post("/login", loginController);
+routes.get("/home", homeController);
 
 export default routes;
